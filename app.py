52e7043d0d76178f94dc24b086b46c59aadaf997
MurshidMera <techmurshid@gmail.com>
2026-02-23 12:56:45 +0530

    updated the file to git

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-print("Hello git configuration")
-print("modification")
+print("hi python");
+pods=["web-1","Web-2","api-1","db-1"]#List example to deploy
+#tuple use , when data is not going to change, e.g connection string , url.
+#Set => when we want to see the existing or not. e.g ip whitelisting or blocklisting. ip is lso not duplicate.
+#print(pods[-2])#Negative indexing allow
+print(pods[1:3])
+pods.append("api-2")# always add at the end.
+pods.insert(2,"db-2")#Add at index.
+pods.extend(["db-3","web-3","api-3"])# add multiple
+print(pods)
+
+#Remove value
+pods.remove("db-1")
+print("After removing",pods)
+#print(pods.count)
+pods.sort()
---
+++ ops.py
@@ -0,0 +1,29 @@
+class BankAccount:
+
+    def __init__(self, name, accno, balance):
+        self.name = name
+        self.accno = accno
+        self.balance = balance
+
+    def show_details(self):
+        print("Name :",self.name)
+        print("Account No :",self.accno)
+        print("Balance :", self.balance)
+
+    def deposit(self,amount):
+        self.balance+=amount
+        print(f"{amount} deposited succssfully ")
+
+    def getBalance(self):
+        return self.balance
+
+
+abash_acc = BankAccount("Abhash", 123412415324, 20000)
+
+sharath_acc = BankAccount("Sharath", 4121313123, 50000)
+
+#print(vars(abash_acc)) #Printing complete object value
+#print(sharath_acc.__dict__) #double underscore obj.__dict__, print complete object value.
+print(sharath_acc.show_details())
+sharath_acc.deposit(5000)
+print("Current Blance ",sharath_acc.getBalance())
